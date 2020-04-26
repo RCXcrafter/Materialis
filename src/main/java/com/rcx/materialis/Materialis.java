3c6ccaa7000af38dd2be60083903e3b71d82782a
RCXcrafter <rcxcrafter@gmail.com>
2020-04-27 00:53:47 +0200

    increase version and add screenshots

--- src/main/java/com/rcx/materialis/Materialis.java
+++ src/main/java/com/rcx/materialis/Materialis.java
@@ -30,7 +30,7 @@ public class Materialis {
 	public static CommonProxy proxy;
 	public static final String ID = "materialis";
 	public static final String NAME = "Materialis";
-	public static final String VERSION = "1.1.0";
+	public static final String VERSION = "1.1.1";
 
 	public static ItemStack tabItem = TinkerCommons.ingotKnightSlime;
 	public static CreativeTab MatTab = new CreativeTab(ID + ".creativeTab", ItemStack.EMPTY) {
