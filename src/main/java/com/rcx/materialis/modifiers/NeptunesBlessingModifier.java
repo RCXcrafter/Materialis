287d8fb10a1141d27b213d1f9d8427442515f9a4
RCXcrafter <rcxcrafter@gmail.com>
2021-11-03 19:09:12 +0100

    account for airborne when cancelling swimming mining modifier

--- src/main/java/com/rcx/materialis/modifiers/NeptunesBlessingModifier.java
+++ src/main/java/com/rcx/materialis/modifiers/NeptunesBlessingModifier.java
@@ -8,6 +8,7 @@ import net.minecraftforge.event.entity.player.PlayerEvent.BreakSpeed;
 import slimeknights.tconstruct.library.modifiers.SingleUseModifier;
 import slimeknights.tconstruct.library.tools.context.ToolAttackContext;
 import slimeknights.tconstruct.library.tools.nbt.IModifierToolStack;
+import slimeknights.tconstruct.tools.TinkerModifiers;
 
 public class NeptunesBlessingModifier extends SingleUseModifier {
 
@@ -25,16 +26,16 @@ public class NeptunesBlessingModifier extends SingleUseModifier {
 	@Override
 	public void onBreakSpeed(IModifierToolStack tool, int level, BreakSpeed event, Direction sideHit, boolean isEffective, float miningSpeedModifier) {
 		if (isEffective)
-			event.setNewSpeed(event.getNewSpeed() * negateWaterMiningModifier(event.getEntityLiving()));
+			event.setNewSpeed(event.getNewSpeed() * negateWaterMiningModifier(event.getEntityLiving(), tool.getModifierLevel(TinkerModifiers.airborne.get()) == 0));
 	}
 
-	public static float negateWaterMiningModifier(LivingEntity entity) {
+	public static float negateWaterMiningModifier(LivingEntity entity, boolean negateSwimming) {
 		float modifier = 1.0f;
 		if (entity.isEyeInFluid(FluidTags.WATER)) {
 			if (!EnchantmentHelper.hasAquaAffinity(entity))
 				modifier *= 5.0F;
 
-			if (!entity.isOnGround())
+			if (!entity.isOnGround() && negateSwimming)
 				modifier *= 5.0F;
 		}
 		return modifier;
