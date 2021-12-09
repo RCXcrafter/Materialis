1581357b67a9eda149cac33b6248c0afefd9ace4
RCXcrafter <rcxcrafter@gmail.com>
2021-12-09 08:26:54 +0100

    fix wrench having wrong amount of slots

--- src/main/java/com/rcx/materialis/datagen/MaterialisToolDefinitions.java
+++ src/main/java/com/rcx/materialis/datagen/MaterialisToolDefinitions.java
@@ -6,6 +6,7 @@ import com.rcx.materialis.modifiers.MaterialisModifiers;
 
 import net.minecraft.data.DataGenerator;
 import slimeknights.tconstruct.library.data.tinkering.AbstractToolDefinitionDataProvider;
+import slimeknights.tconstruct.library.tools.SlotType;
 import slimeknights.tconstruct.library.tools.stat.ToolStats;
 import slimeknights.tconstruct.tools.TinkerModifiers;
 import slimeknights.tconstruct.tools.TinkerToolParts;
@@ -28,7 +29,8 @@ public class MaterialisToolDefinitions extends AbstractToolDefinitionDataProvide
 		.multiplier(ToolStats.ATTACK_SPEED, 1.8f)
 		.multiplier(ToolStats.MINING_SPEED, 2.0f)
 		.multiplier(ToolStats.DURABILITY, 1.5f)
-		.smallToolStartingSlots()
+		.startingSlots(SlotType.UPGRADE, 1)
+		.startingSlots(SlotType.ABILITY, 3)
 		//traits
 		.trait(MaterialisModifiers.wrenchingModifierHidden);
 
@@ -44,7 +46,8 @@ public class MaterialisToolDefinitions extends AbstractToolDefinitionDataProvide
 		.multiplier(ToolStats.ATTACK_SPEED, 1.1f)
 		.multiplier(ToolStats.MINING_SPEED, 1.5f)
 		.multiplier(ToolStats.DURABILITY, 2.5f)
-		.largeToolStartingSlots()
+		.startingSlots(SlotType.UPGRADE, 1)
+		.startingSlots(SlotType.ABILITY, 2)
 		// traits
 		.trait(MaterialisModifiers.wrenchingModifierHidden)
 		.trait(TinkerModifiers.twoHanded);
